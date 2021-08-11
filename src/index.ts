7dece55a094ff02a4e77c5c7acebd8c249c3b0c2
Tim <harvard90873@gmail.com>
2021-08-11 19:11:54 +0200

    initialize inquirer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
 #!/usr/bin/env node
 
-import fs from "fs";
-import inquirer from "inquirer";
+// import fs from "fs";
+import inquirer, { Inquirer } from "inquirer";
+
+const PASSWORD_CUSTOMIZATION_OPTIONS = {
+  num: "with-numbers",
+  upper: "with-uppercase",
+  special: "with-special-characters",
+  toFile: "write-to-file",
+};
+
+interface InquirerResponse {
+  options: string[];
+  length: number;
+}
+
+const buildReference = (options: string[]) => {};
 
 (async () => {
-  console.log("Hello world");
+  const { options, length }: InquirerResponse = await inquirer.prompt([
+    {
+      type: "checkbox",
+      message: "Customize password:",
+      name: "option",
+      choices: Object.values(PASSWORD_CUSTOMIZATION_OPTIONS),
+    },
+    {
+      type: "number",
+      message: "Minimum length:",
+      name: "length",
+      default: 12,
+    },
+  ]);
+
+  // write password to file
+  if (options.indexOf(PASSWORD_CUSTOMIZATION_OPTIONS.toFile)) {
+  }
 })();
